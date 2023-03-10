0b20043dd642e7d8544e848cde9745689ecf1457
Bertrand <mutanganabertrand@gmail.com>
2023-03-10 13:46:00 +0200

    Add

---
+++ controllers/controller.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const axios = require('axios');
+const data = require('../data/currency-symbols')
+
+const getCurrencySymbols = (req, res) => {
+  res.render('index', {currencySymbols: data});
+}
+
+const currencyConvert = async (req,res) => {
+    const { fromCurrency, toCurrency, amount } = req.query;
+    try {
+        console.log(process.env.API_KEY)
+      const response = await axios.get(`https://api.apilayer.com/fixer/convert?to=${toCurrency}&from=${fromCurrency}&amount=${amount}`, {
+        headers: {
+          apikey: `${process.env.API_KEY}`,
+        }
+      });
+      const result = response.data;
+      res.render('result', { amount, fromCurrency, toCurrency, result });
+    } catch (error) {
+      console.error(error);
+      res.render('error');
+    }
+}
+
+module.exports = [currencyConvert, getCurrencySymbols]
